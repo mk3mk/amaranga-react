a5103b53f71362def594284ebd522d3bc3a3c216
mk3mk <mk3mk@yandex.ru>
2024-03-13 18:41:20 +0300

    5555

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -127,6 +127,9 @@ function Dashboard() {
         <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
           <h1 className="h2">My Dashboard</h1>
           <span>Hello {username}!</span>
+          <span>Hello {full_name}!</span>
+          <span>Hello {user_id}!</span>
+          <span>Hello {image}!</span>
           <div className="btn-toolbar mb-2 mb-md-0">
             <div className="btn-group mr-2">
               <button className="btn btn-sm btn-outline-secondary">
